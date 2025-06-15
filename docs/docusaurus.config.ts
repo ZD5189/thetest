a8d90d27dfd73da0bf0cc5e2e7c70d0479c3391d
zd9503 <zd5189@gmail.com>
2025-06-15 17:08:18 +0800

    推送测试

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site zdd',
+  title: 'My Site web',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
