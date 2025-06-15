ee4ae0a3fdc9a65f092d3b07cec4af236ebec095
zd9503 <zd5189@gmail.com>
2025-06-15 17:13:02 +0800

    推送测试

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://zd5189.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thetest/',
   // deploymentBranch:'gh-pages',
 
   // GitHub pages deployment config.
